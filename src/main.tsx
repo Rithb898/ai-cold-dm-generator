0afda2bb385f2284e9d72501b6e234960d4e6fa5
Rithb898 <105077299+Rithb898@users.noreply.github.com>
2025-06-06 19:40:38 +0530

    feat: integrate PostHog for analytics tracking

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,22 @@ import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import { Toaster } from "react-hot-toast";
+import { PostHogProvider } from "posthog-js/react";
+
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: "2025-05-24",
+};
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-      <App />
+      <PostHogProvider
+        apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+        options={options}
+      >
+        <App />
+      </PostHogProvider>
       <Toaster position='top-center' reverseOrder={false} />
     </ThemeProvider>
   </StrictMode>
